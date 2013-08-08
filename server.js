db66e502797f39be38bddb50e575bdf5a6ebddbd
oroce <robert@oroszi.net>
2013-08-08 17:47:01 +0200

    fixed whitespace

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var ports = seaport.createServer();
 
 
 ports.listen( 9090, function( err ){
-	if( err ){
-		return console.error( "Server cannot be started!", err );
-	}
-	console.log( "listening on: %s:%s", ports.address().address, ports.address().port );
+  if( err ){
+    return console.error( "Server cannot be started!", err );
+  }
+  console.log( "listening on: %s:%s", ports.address().address, ports.address().port );
 });
