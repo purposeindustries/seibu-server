3adeaa96bb6275a1e3936049a2608c2e40321847
oroce <robert@oroszi.net>
2013-08-08 17:46:08 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var seaport = require( "seaport" );
+
+var ports = seaport.createServer();
+
+
+ports.listen( 9090, function( err ){
+	if( err ){
+		return console.error( "Server cannot be started!", err );
+	}
+	console.log( "listening on: %s:%s", ports.address().address, ports.address().port );
+});
